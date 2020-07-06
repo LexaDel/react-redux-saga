9cc126c007de2a4dd136aa7ca7f1944427d7d7bf
Alexey Delezhov <lexadelejov@gmail.com>
2020-07-06 19:09:21 +0300

    improve: add delete and getData on the server

--- routes/api.js
+++ routes/api.js
@@ -3,10 +3,27 @@ const fetch = require('node-fetch');
 
 const router = express.Router();
 
+let todos;
+
 router.get('/data', async (req, res) => {
-    const data = await fetch('https://jsonplaceholder.typicode.com/todos')
-        .then(res => res.json())
-    res.json(data);
+    todos = await fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(res => res.json());
+    console.log('data');
+    res.json(todos);
+});
+
+router.get('/getData', async (req, res) => {
+    res.json(todos);
+});
+
+router.delete('/delete/:todoId', async (req, res) => {
+    try {
+        const { todoId } = req.params;
+        todos = todos.filter(todo => todo.id != todoId);
+        res.json({STATE: "SUCCESS"});
+    } catch (err) {
+        res.json({STATE: "FAILED"});
+    }
 });
 
 module.exports = router;
